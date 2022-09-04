730c5f5dcc358eaa866e97437dafd5379f613d72
LimpidSunset <danmrobinson@protonmail.com>
2022-09-04 17:42:36 +0100

    Update melter.py

--- melter.py
+++ melter.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import re
 
+st.set_page_config(page_icon="🫕", page_title="The Melter")
+
 def _max_width_():
 	max_width_str = f"max-width: 1800px;"
 	st.markdown(
@@ -14,8 +16,6 @@ def _max_width_():
 	""",
 		unsafe_allow_html=True,
 	)
-
-st.set_page_config(page_icon="🫕", page_title="The Melter")
 
 st.image(
 	"https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/fondue_1fad5.png",
