ce5342690ceed26c5142e5265f613f7f0d88e68f
Dan Robinson <danmrobinson@protonmail.com>
2023-03-17 11:52:35 +0000

    Update melter.py

--- melter.py
+++ melter.py
@@ -18,7 +18,7 @@ def _max_width_():
 	)
 
 st.image(
-	"https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/fondue_1fad5.png",
+	"https://em-content.zobj.net/thumbs/240/apple/325/fondue_1fad5.png",
 	width=100,
 )
 
